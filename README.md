a7fea1bef468f34dc2388d130622ba58b51d9d37
Funto <funto.afuwape@gmail.com>
2023-09-26 12:30:44 +0100

    Create board class
    This class initializes an array of rows and columns.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # knight_travails
+
+This project is a part [The Odin Project](https://www.theodinproject.com/)'s curriculum.
---
+++ knight_travails.rb
@@ -0,0 +1,6 @@
+class Board
+  def initialize
+    @rows = [0, 1, 2, 3, 4, 5, 6, 7]
+    @columns = [0, 1, 2, 3, 4, 5, 6, 7]
+  end
+end

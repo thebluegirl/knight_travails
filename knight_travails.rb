04b99f86619f1e75e7a3065039716d9da0342798
Funto <funto.afuwape@gmail.com>
2023-09-26 12:39:56 +0100

    Create knight class
    This class initializes with the location of the knight represented by an array with two values.

--- knight_travails.rb
+++ knight_travails.rb
@@ -1,6 +1,17 @@
 class Board
+  attr_reader :rows, :columns
   def initialize
     @rows = [0, 1, 2, 3, 4, 5, 6, 7]
     @columns = [0, 1, 2, 3, 4, 5, 6, 7]
   end
+end
+
+class Knight
+  attr_reader :location
+  def initialize
+    @location = Array.new(2)
+  end
+
+  protected
+  attr_accessor :location
 end

7be9512c7dccc25213ba66f443cbb78ad29f45db
Funto <funto.afuwape@gmail.com>
2023-09-29 10:41:09 +0100

    Create Board#squares
    This method calls the Board#squares_builder method on that boards instance of rows and columns and uses iteration to create the 64 squares of the chess board.

--- knight_travails.rb
+++ knight_travails.rb
@@ -1,9 +1,26 @@
 class Board
-  attr_reader :rows, :columns
+  attr_reader :squares
   def initialize
     @rows = [0, 1, 2, 3, 4, 5, 6, 7]
     @columns = [0, 1, 2, 3, 4, 5, 6, 7]
+    @squares = squares_builder(@rows, @columns)
   end
+
+  def squares_builder(arr1, arr2)
+    squares = Array.new
+    arr1.each do |row|
+      arr2.each do |column|
+        coordinates = []
+        coordinates << row
+        coordinates << column
+        squares << coordinates
+      end
+    end
+    return squares
+  end
+
+  protected
+  attr_accessor :row, :columns
 end
 
 class Knight

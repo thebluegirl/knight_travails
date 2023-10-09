d016219d981b477aade7fb99eb8dfb324990413e
Funto <funto.afuwape@gmail.com>
2023-10-09 14:48:45 +0100

    Modify knight class to return path
    Knight#path was created and Knight#move_counter modified to return the path taken.
    
    Vertex#path_taken was also created to facilitate this.
    
    The result seems a little buggy, especially when the path is rather long, so it may be necessary to refactor or debug the code later.

--- knight_travails.rb
+++ knight_travails.rb
@@ -1,8 +1,10 @@
 class Vertex
+  attr_accessor :path_taken
   attr_reader :square, :possible_moves
   def initialize(square)
     @square = square
     @possible_moves = possible_moves_finder(@square)
+    @path_taken = []
   end
 
   def possible_moves_finder(square)
@@ -71,18 +73,39 @@ class Knight
     @location = location
     @location_on_board = board.vertex_finder(location)
     @destination = destination
+    @path = [@location]
   end
 
   def move_counter
     counter = 0
     return counter if @location == @destination
-    return (counter + 1) if @location_on_board.possible_moves.include?(@destination)
+    if @location_on_board.possible_moves.include?(@destination)
+      @path << @destination
+      return (counter + 1)
+    end
+
     def recurse(queue=@board.possible_moves_on_board(@location_on_board), counter=2)
       next_queue = []
-      queue.each do |square|
-        return counter if square.possible_moves.include?(@destination)
+      queue.each do |square|      
+        if square.possible_moves.include?(@destination)
+          square.path_taken.each do |coordinates|
+            @path << coordinates
+          end
+          @path << square.square
+          @path << @destination
+          return counter
+        end
         square.possible_moves.each do |possible_move|
           next_queue << @board.vertex_finder(possible_move)
+          if square.path_taken.empty?
+            @board.vertex_finder(possible_move).path_taken << square.square if !@board.vertex_finder(possible_move).path_taken.include?(square.square)
+          else
+            @board.vertex_finder(possible_move).path_taken = square.path_taken
+            @board.vertex_finder(possible_move).path_taken << square.square if !@board.vertex_finder(possible_move).path_taken.include?(square.square)
+          end
+          until @board.vertex_finder(possible_move).path_taken.size < counter
+            @board.vertex_finder(possible_move).path_taken.pop
+          end
         end
       end
       counter += 1
@@ -91,12 +114,16 @@ class Knight
     recurse
   end
 
+  def path_printer
+    self.path.each { |coordinates| puts "#{coordinates}" }
+  end
+
   protected 
-  attr_accessor :board
+  attr_accessor :board, :path
 end
 
 def knight_moves(location, destination)
   knight = Knight.new(location, destination)
-  move_count = knight.move_counter
-  puts "You made it in #{move_count} moves!"
+  puts "You made it in #{knight.move_counter} moves!"
+  knight.path_printer
 end
